7916a73bb355ed5cf14a4d0af8969fc06ee7be6f
boggs <hello@boggs.xyz>
2017-03-05 01:57:57 +0800

    Configure cors

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,14 @@ module Rails5ApiDeviseTokenAuthDemo
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+          :headers => :any,
+          :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+          :methods => [:get, :post, :options, :delete, :put]
+      end
+    end
   end
 end
